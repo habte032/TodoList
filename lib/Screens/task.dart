280ea8d5c76cf0d2a409eb0aa82339ddea64edea
habte032 <habte032@gmail.com>
2023-06-28 21:19:59 +0300

    Extract ListView and ListTile

--- lib/Screens/task.dart
+++ lib/Screens/task.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class taskScreen extends StatelessWidget {
 
-   bool ischeked=false;
+  bool ischeked=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,62 +12,93 @@ class taskScreen extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              flex: 2,
+                flex: 2,
                 child:
-               Container(
-                 decoration: BoxDecoration(
-                     color: Color(0xff59c1ff),
-                   //borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(10))
-                 ),
-                 width: double.infinity,
-              padding: const EdgeInsets.only(top: 25,left: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-
-                children: [
-                  CircleAvatar(
-                   backgroundColor: Colors.white,
-                   child: IconButton(onPressed: (){
-
-                   }, icon: Icon(Icons.list,color: Color(0xff59c1ff),),),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff59c1ff),
+                    //borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(10))
                   ),
-                  SizedBox(height: 10,),
-                  Text('Todoey',style: TextStyle(fontSize: 40,color: Colors.white,fontWeight: FontWeight.w700),),
-                  Text('12 Tasks',style: TextStyle(color: Colors.white,fontSize: 18),)
-                ],
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(top: 25,left: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
 
-              ),
-            )),
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: IconButton(onPressed: (){
+
+                        }, icon: Icon(Icons.list,color: Color(0xff59c1ff),),),
+                      ),
+                      SizedBox(height: 10,),
+                      Text('Todoey',style: TextStyle(fontSize: 40,color: Colors.white,fontWeight: FontWeight.w700),),
+                      Text('12 Tasks',style: TextStyle(color: Colors.white,fontSize: 18),)
+                    ],
+
+                  ),
+                )),
             Expanded(
-              flex: 5,
+                flex: 5,
                 child: Container(
                   width: double.infinity,
                   decoration: BoxDecoration(
                       color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20))
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20))
                   ),
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(
-
-                children: [
-                 ListTile(
-                   title: Text('new task',style: TextStyle(),),
-                 )
-                ],
-
-              ),
-            ))
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child:
+                  TaskList(ischeked: ischeked),
+                ))
           ],
         ),
       ),
       floatingActionButton:
       FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
-          onPressed: (){
-      },
-      child: Icon(Icons.add,color: Colors.white,),),
+        onPressed: (){
+        },
+        child: Icon(Icons.add,color: Colors.white,),),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  const TaskList({
+    super.key,
+    required this.ischeked,
+  });
+
+  final bool ischeked;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        TaskTile(ischeked: ischeked),
+        TaskTile(ischeked: ischeked),
+        TaskTile(ischeked: ischeked),
+        TaskTile(ischeked: ischeked),
+      ],
+
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({required this.ischeked});
+
+  final bool ischeked;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      trailing: Checkbox(value: ischeked,onChanged: (value){
+
+      },),
+      title: Text('New task',style: TextStyle(),),
     );
   }
 }
