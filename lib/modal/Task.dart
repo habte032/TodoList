845e0bbba2b41c9283862033477599e6878d2e73
habte032 <habte032@gmail.com>
2023-06-29 21:49:13 +0300

    function to task Checkbox

---
+++ lib/modal/Task.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Task{
+  final String name;
+  bool isDone;
+  Task({required this.name,required this.isDone});
+
+  void togglecheck(){
+    isDone=!isDone;
+}
+}
