3d37e815522e0e772257f7f7d399c4f89de800e1
habte032 <habte032@gmail.com>
2023-06-29 21:50:44 +0300

    update to add new task

--- lib/Screens/task.dart
+++ lib/Screens/task.dart
@@ -1,9 +1,19 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class taskScreen extends StatelessWidget {
+import '../modal/Task.dart';
+import 'BottomSheet.dart';
 
-  bool ischeked=false;
+class taskScreen extends StatefulWidget {
+
+  @override
+  State<taskScreen> createState() => _taskScreenState();
+}
+
+class _taskScreenState extends State<taskScreen> {
+
+  List<Task> tasks=[
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,33 +21,30 @@ class taskScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(
-                flex: 2,
-                child:
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xff59c1ff),
-                    //borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(10))
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xff59c1ff),
+                //borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(10))
+              ),
+              width: double.infinity,
+              padding: const EdgeInsets.only(top: 25,left: 15,bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: IconButton(onPressed: (){
+
+                    }, icon: Icon(Icons.list,color: Color(0xff59c1ff),),),
                   ),
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(top: 25,left: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  SizedBox(height: 10,),
+                  Text('Todoey',style: TextStyle(fontSize: 40,color: Colors.white,fontWeight: FontWeight.w700),),
+                  Text('${tasks.length}',style: TextStyle(color: Colors.white,fontSize: 18),)
+                ],
 
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        child: IconButton(onPressed: (){
-
-                        }, icon: Icon(Icons.list,color: Color(0xff59c1ff),),),
-                      ),
-                      SizedBox(height: 10,),
-                      Text('Todoey',style: TextStyle(fontSize: 40,color: Colors.white,fontWeight: FontWeight.w700),),
-                      Text('12 Tasks',style: TextStyle(color: Colors.white,fontSize: 18),)
-                    ],
-
-                  ),
-                )),
+              ),
+            ),
             Expanded(
                 flex: 5,
                 child: Container(
@@ -49,8 +56,9 @@ class taskScreen extends StatelessWidget {
                           topRight: Radius.circular(20))
                   ),
                   padding: const EdgeInsets.only(top: 20.0),
-                  child:
-                  TaskList(ischeked: ischeked),
+                  child:tasks.length==0?Center(child: Text('No task available here'
+                      '\n Add new Task',textAlign: TextAlign.center,),):
+                  TaskList(tasks),
                 ))
           ],
         ),
@@ -58,48 +66,77 @@ class taskScreen extends StatelessWidget {
       floatingActionButton:
       FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
-        onPressed: (){
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) => SingleChildScrollView(
+                  child:Container(
+                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                    child: AddTask((newTask){
+                      print(newTask);
+                      setState(() {
+                        tasks.add(Task(name: newTask, isDone: false),);
+                      });
+
+
+                    } ),
+                  )
+              )
+          );
         },
         child: Icon(Icons.add,color: Colors.white,),),
     );
   }
 }
 
-class TaskList extends StatelessWidget {
-  const TaskList({
-    super.key,
-    required this.ischeked,
-  });
+class TaskList extends StatefulWidget {
+  TaskList(this.tasks);
+  List<Task> tasks;
 
-  final bool ischeked;
+
+  @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(ischeked: ischeked),
-        TaskTile(ischeked: ischeked),
-        TaskTile(ischeked: ischeked),
-        TaskTile(ischeked: ischeked),
-      ],
-
-    );
+    return ListView.builder(
+      itemCount: widget.tasks.length,
+      itemBuilder: (context, index){
+        return TaskTile(isChecked: widget.tasks[index].isDone,
+            title: widget.tasks[index].name,
+          toggleCheckBoxState: (checkboxState){
+               setState(() {
+                 widget.tasks[index].togglecheck();
+               });
+          },
+        );
+      });
   }
 }
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({required this.ischeked});
+class TaskTile extends StatelessWidget{
+  final  bool isChecked;
+  final String title;
+  final Function toggleCheckBoxState;
 
-  final bool ischeked;
+  TaskTile({required this.isChecked, required this.title,required this.toggleCheckBoxState});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      trailing: Checkbox(value: ischeked,onChanged: (value){
-
-      },),
-      title: Text('New task',style: TextStyle(),),
+      trailing: Checkbox(
+        value: isChecked,
+        onChanged: (newValue) => toggleCheckBoxState(newValue!),
+      ),
+      title: Text(title,style: TextStyle(decoration: isChecked?TextDecoration.lineThrough:null),),
     );
   }
 }
+
+
 
