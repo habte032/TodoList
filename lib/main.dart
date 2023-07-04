c86d270d57d198ad86910477623c2d6ddedbe837
habte032 <habte032@gmail.com>
2023-07-04 13:47:46 +0300

    add provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-
 import 'Screens/task.dart';
+import 'package:provider/provider.dart';
+import 'modal/task_data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false ,
-      home: taskScreen(),
+    return ChangeNotifierProvider(
+      create: (_) => TaskData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: taskScreen(),
+      ),
     );
   }
 }
+
+
